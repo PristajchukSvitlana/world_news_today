23e9e9ea501b7d04181f6f3df96556f6b11a6154
Svitlana Pristajchuk <spristajchuk@gmail.com>
2025-11-16 13:43:28 +0100

    creat docks

--- world_news_today/vite.config.js
+++ world_news_today/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/world_news_today/',          // назва репозиторію
+  build: {
+    outDir: 'docs',                    // ⚠️ будувати у папку docs
+  },
+});
+
